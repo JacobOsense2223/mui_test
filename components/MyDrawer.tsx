5cc99a5473211e35b4f029c87975e1535af0c0c6
jacob_jheng <jacob@osensetech.com>
2024-01-19 15:19:26 +0800

    fix code

--- components/MyDrawer.tsx
+++ components/MyDrawer.tsx
@@ -10,11 +10,9 @@ export const MyDrawer = () => {
             <MenuIcon></MenuIcon>
         </IconButton>
         <Drawer anchor='left' open={isDrawerOpen} onClose={() => setIsDrawerOpen(false)}>
-            <Box p={2} width={'250px'} textAlign={'center'} role='presentation'>
-                <Typography variant='h6' component={'div'}>
-                    Side Panel
-                </Typography>
-            </Box>
+            <Typography variant='h6' component={'div'}>
+                Side Panel
+            </Typography>
         </Drawer>
     </>
     )
